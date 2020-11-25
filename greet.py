eae04cbe2cee4491b0e2931ed906852df1db0618
Roman-kazi <rpgk2016@gmail.com>
2020-11-25 16:50:31 +0530

    used revert to go back to first commit

--- greet.py
+++ greet.py
@@ -1,12 +1,6 @@
 
 
-def greet(name):
-    print("Hello,",name)
 
-name = input("What is your name?")
-greet(name)
-
-def play():
-    print("Playing a video")
-
-play()
+def greet():
+    print("Hello xyz")
+greet()
